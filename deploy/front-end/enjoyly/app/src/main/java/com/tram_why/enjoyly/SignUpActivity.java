4e8208f737274d65b8113018b4edb8b2a82e1fcc
Valentin Shevchuk <simfoner@i.ua>
2017-03-25 19:12:49 +0200

    Add sign Up request

--- deploy/front-end/enjoyly/app/src/main/java/com/tram_why/enjoyly/SignUpActivity.java
+++ deploy/front-end/enjoyly/app/src/main/java/com/tram_why/enjoyly/SignUpActivity.java
@@ -1,13 +1,74 @@
 package com.tram_why.enjoyly;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    EditText etEmail;
+    EditText etPassword;
+    EditText etFirstName;
+    EditText etLastName;
+    Button bSignUp;
+    TextView tSignOnLink;
+    String email;
+    String password;
+    String firstName;
+    String lastName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        etFirstName = (EditText) findViewById(R.id.etFirstName);
+        etLastName = (EditText) findViewById(R.id.etLastName);
+        bSignUp = (Button) findViewById(R.id.bSignUp);
+        tSignOnLink = (TextView) findViewById(R.id.tSignUpLink);
+
+
+        bSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email = etEmail.getText().toString();
+                password = etPassword.getText().toString();
+                firstName = etFirstName.getText().toString();
+                lastName = etLastName.getText().toString();
+                Request request = new Request();
+                request.execute();
+            }
+        });
+        tSignOnLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+    class Request extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            Services services = new Services();
+            services.SignUp(firstName, lastName, email, password);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+        }
     }
 }
